f1a08623579489cbec16e508646ddc5755b1f73d
tdrevitchvgrdn <110933291+timdrevitch@users.noreply.github.com>
2022-09-20 21:41:38 -0400

    📵 Adding a request rate limit

--- server.js
+++ server.js
@@ -6,16 +6,27 @@ const path = require("path")
 const route = require("./Routes/routes")
 const PORT = process.env.PORT || 4000
 const app = express()
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require("express-rate-limit")
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10,
+})
+
+// apply rate limiter to all requests
+app.use(limiter)
+
 app.use(cors())
 app.use(express.static(path.join(__dirname, "client", "build")))
 
 //connect to the db
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 mongoose.connection.on("connected", () => {
-    console.log("Connected to the MongoDB database!")
+  console.log("Connected to the MongoDB database!")
 })
 
 //data parsing
@@ -26,9 +37,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use("/api/", route)
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 
-app.listen( PORT, () => {
-    console.log(`Connected to server on port ${PORT}!`)
+app.listen(PORT, () => {
+  console.log(`Connected to server on port ${PORT}!`)
 })
